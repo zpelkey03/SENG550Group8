07f4c978437d9c47f140df26be831a75329de0f0
Zach Pelkey <113135691+zpelkey03@users.noreply.github.com>
2025-09-17 15:30:24 -0600

    update: added questions 1-6 work

---
+++ Assignment1/group8.sql
@@ -0,0 +1,98 @@
+
+-- Table cleanup for assignment
+DROP TABLE IF EXISTS students CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+
+-- Question 1 - 
+CREATE TABLE students (
+	id 		SERIAL 	PRIMARY KEY,
+	name 	TEXT 	NOT NULL,
+	age		INT		NOT NULL,
+	major 	TEXT	NOT NULL
+);
+
+INSERT INTO students (name, age, major) VALUES
+('John', 19, 'Arts'),
+('Mark', 27, 'Computer Science'),
+('Luke', 21, 'Engineering'),
+('Matthew', 18, 'Engineering');
+
+--Question 1 Queries
+SELECT * FROM students;
+
+SELECT * FROM students WHERE age > 21;
+
+SELECT name FROM students WHERE major = 'Computer Science';
+
+--Question 2
+ALTER TABLE students
+ADD COLUMN email VARCHAR(100);
+
+UPDATE students
+SET email = 'john@icloud.com'
+WHERE name = 'John';
+
+UPDATE students
+SET email = 'mark@icloud.com'
+WHERE name = 'Mark';
+
+SELECT name, email FROM students
+
+--Question 3
+SELECT * FROM STUDENTS
+
+SELECT COUNT('*') FROM STUDENTS
+
+-- Question 4
+CREATE TABLE products(
+	product_id 	int UNIQUE,
+	name 	TEXT 	NOT NULL,
+	price	FLOAT   NOT NULL
+);
+
+COPY products(product_id, name, price)
+FROM 'C:\SENG550\Group8Repo\Assignment1\products.csv'
+DELIMITER ','
+CSV HEADER;
+
+SELECT * FROM products
+
+SELECT * FROM products WHERE price < 2.00;
+
+SELECT MAX(price) as MaximumPrice FROM products;
+
+--Question 5
+DELETE FROM students WHERE name = 'John';
+
+SELECT * FROM students ORDER BY AGE DESC;
+
+SELECT * 
+FROM students
+WHERE age  = (SELECT(MIN(age)) FROM students)
+
+--Question 6
+SELECT major, COUNT ('*') FROM students GROUP BY major;
+
+SELECT AVG(price) as averageprice from products;
+
+SELECT SUM(price) as Total from products;
+
+-- Question 7 - 
+CREATE TABLE enrollments (
+	enroll_id 	SERIAL 	PRIMARY KEY,
+	student_id	INT REFERENCES students(id),
+	course		TEXT	NOT NULL
+);
+
+INSERT INTO enrollments (student_id, course) VALUES
+
+INSERT INTO students (name, age, major) VALUES
+('John', 19, 'Arts'),
+('Mark', 27, 'Computer Science'),
+('Luke', 21, 'Engineering'),
+('Matthew', 18, 'Engineering');
+
+
+
+
+
