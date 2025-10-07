71d05020830d8c2acb207cd064ad8887690f7140
Zach Pelkey <113135691+zpelkey03@users.noreply.github.com>
2025-10-07 17:32:53 -0600

    addition: added A2_Pt1 sql file

---
+++ Assignment2/A2_P1.sql
@@ -0,0 +1,114 @@
+
+-- Drop Tables if they exists
+DROP TABLE dim_customers CASCADE;
+DROP TABLE IF EXISTS dim_products CASCADE;
+DROP TABLE IF EXISTS fact_orders CASCADE;
+
+-- 1.1 A - 
+CREATE TABLE dim_customers (
+	id 		BIGSERIAL 	PRIMARY KEY,
+	customer_id TEXT NOT NULL,
+	name 	TEXT 	NOT NULL,
+	email	TEXT	NOT NULL,
+	city	TEXT	NOT NULL,
+	valid_from 	TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
+	valid_to	TIMESTAMPTZ,
+	is_current BOOLEAN NOT NULL DEFAULT TRUE
+);
+CREATE INDEX idx_dim_customers_current
+	ON dim_customers(customer_id, is_current);
+
+-- 1.1 B --
+CREATE TABLE dim_products (
+	id 		BIGSERIAL 	PRIMARY KEY,
+	product_id TEXT NOT NULL,
+	name 	TEXT 	NOT NULL,
+	category TEXT	NOT NULL,
+	price	NUMERIC(10,2)	NOT NULL,
+	valid_from 	TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
+	valid_to	TIMESTAMPTZ,
+	is_current BOOLEAN NOT NULL DEFAULT TRUE
+);
+CREATE INDEX idx_dim_products_current
+	ON dim_products(product_id, is_current);
+
+-- 1.1 C --
+CREATE TABLE fact_orders (
+	order_id TEXT PRIMARY KEY,
+	customer_id TEXT NOT NULL,
+	product_id TEXT NOT NULL,
+	order_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	amount NUMERIC(10,2) NOT NULL
+);
+
+
+--P1.2 1. --
+INSERT INTO dim_customers (customer_id, name, email, city, valid_from)
+VALUES
+	('C1', 'Alice', 'alice@icloud.com', 'New York', '2025-10-07 11:00-06'),
+	('C2', 'Bob', 'bob@icloud.com', 'Boston', '2025-10-07 11:00-06');
+
+INSERT INTO dim_products (product_id, name, category, price, valid_from)
+VALUES
+	('P1', 'Laptop', 'Computing Devices', 1000, '2025-10-07 11:00-06'),
+	('P2', 'Phone', 'Mobile Devices', 500, '2025-10-07 11:00-06');
+
+--P1.2 2. --
+INSERT INTO fact_orders
+VALUES ('O1', 'C1', 'P1', '2025-10-07 11:05-06', 1000);
+
+--P1.2 3. --
+UPDATE dim_customers
+	SET valid_to='2025-10-07 11:05-06', is_current = FALSE
+WHERE customer_id='C1' AND is_current=TRUE;
+
+INSERT INTO dim_customers (customer_id, name, email, city, valid_from, is_current)
+SELECT customer_id, name, email, 'Chicago', '2025-10-07 11:05-06', TRUE
+FROM dim_customers
+WHERE customer_id='C1'
+ORDER BY id DESC
+LIMIT 1;
+
+--P1.2 4. --
+UPDATE dim_products
+	SET valid_to='2025-10-07 11:05-06', is_current = FALSE
+WHERE product_id='P1' AND is_current=TRUE;
+
+INSERT INTO dim_products (product_id, name, category, price, valid_from, is_current)
+SELECT product_id, name, category, 900, '2025-10-07 11:10-06', TRUE
+FROM dim_products
+WHERE product_id='P1'
+ORDER BY id DESC
+LIMIT 1;
+
+--P1.2 5. --
+INSERT INTO fact_orders
+VALUES ('O2', 'C1', 'P1', '2025-10-07 11:15-06', 850);
+
+--P1.2 6. --
+UPDATE dim_customers
+	SET valid_to='2025-10-07 11:15-06', is_current = FALSE
+WHERE customer_id='C2' AND is_current=TRUE;
+
+INSERT INTO dim_customers (customer_id, name, email, city, valid_from, is_current)
+SELECT customer_id, name, email, 'Calgary', '2025-10-07 11:15-06', TRUE
+FROM dim_customers
+WHERE customer_id='C2'
+ORDER BY id DESC
+LIMIT 1;
+
+--P1.2 7. --
+INSERT INTO fact_orders
+VALUES ('O3', 'C2', 'P2', '2025-10-07 11:15-06', 500);
+--P1.2 8. --
+INSERT INTO fact_orders
+VALUES ('O4', 'C1', 'P1', '2025-10-07 11:15-06', 900);
+
+-- Queries to see tables
+SELECT * FROM fact_orders ORDER BY order_date ASC;
+-- SELECT * FROM dim_customers;
+-- SELECT * FROM dim_products;
+
+
+
+	
